5bb075895687f94dd9ebcecd75ca3a93f8ae7ed1
LvPin <lp871681354@126.com>
2021-12-05 19:25:54 -0500

    Update Muti_process.R

--- Muti_process.R
+++ Muti_process.R
@@ -1,6 +1,5 @@
 #### functions for Multiome cellranger ####
 
-
 #### matrix_folder: filtered_feature_bc_matrix folder output by cellranger #####
 #### output_tags: sample name #####
 #### MT_tags: genes which are located in Mitochondria #####
@@ -38,3 +37,34 @@ Muti_process_S1 <- function(matrix_folder,output_folder,output_tags,MT_tags){
 	raw_file = paste(output_tags,'Seurat_RNA_raw',sep='_')
 	saveRDS(RNA_Seurat,file=raw_file)
 }
+
+
+#### output_folder in step1 #####
+#### output_tags in step1 #######
+
+Muti_process_S2 <- function(output_folder,output_tags,nFeature_RNA,nCount_RNA,mt_RNA){
+	library(Seurat)
+	#######
+	setwd(output_folder)
+	raw_file = paste(output_tags,'Seurat_RNA_raw',sep='_')
+	print(raw_file)
+	#######
+	RNA_Seurat = readRDS(raw_file)
+	#######
+	k_nFeature = which(RNA_Seurat$nFeature_RNA > nFeature_RNA[1] & RNA_Seurat$nFeature_RNA < nFeature_RNA[2])
+	k_nCount = which(RNA_Seurat$nCount_RNA > nCount_RNA[1] & RNA_Seurat$nCount_RNA < nCount_RNA[2])
+	k_mt = which(RNA_Seurat$percent.mt > mt_RNA[1] & RNA_Seurat$percent.mt < mt_RNA[2])
+	#######
+	k_merge = intersect(k_nFeature,k_nCount)
+	k_merge = intersect(k_merge,k_mt)
+	#######
+	RNA_Seurat_cl1 = RNA_Seurat[,k_merge]
+	#######
+	print(paste('Cells remain', round(dim(RNA_Seurat_cl1)[2]/dim(RNA_Seurat)[2],2)))
+	###### save the cleaned data seurat ######
+	clean_file = paste(output_tags,'Seurat_RNA_clean',sep='_')
+	saveRDS(RNA_Seurat_cl1,file=clean_file)
+	#######
+}
+
+
