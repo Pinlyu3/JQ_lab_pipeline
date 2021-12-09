6087b7d8d7809ebb5c69248fb2f607a873e15092
LvPin <lp871681354@126.com>
2021-12-09 02:14:02 -0500

    Update Muti_velocity.py

--- Muti_velocity.py
+++ Muti_velocity.py
@@ -14,13 +14,15 @@ p = argparse.ArgumentParser(usage = "Get parameter", description = "velocity")
 p.add_argument('--output_folder')
 p.add_argument('--metadata')
 p.add_argument('--loomfile')
+p.add_argument('--output_tags')
+
 
 args = p.parse_args()
 
 metadata = args.metadata
-index = args.
 output_folder = args.output_folder
 loomfile =  args.loomfile
+output_tags = args.output_tags
 #######
 #######
 #######
@@ -48,7 +50,74 @@ for i in cell_id:
 print('trouble shooting: cell_id format in metadata is ' + cell_id_new[1])
 #### 
 
+#### cell_id in data format ########
+
 Cell_id_all = data.obs.index
 Cell_id_all = pd.Series(Cell_id_all,dtype=object)
-index = Cell_id_all.isin(cell_id_new)
-adata = adata[index,:]
+Cell_id_all_list = Cell_id_all.tolist()
+
+Cell_id_all_new = list()
+
+for j in Cell_id_all_list:
+	j_new = j.split(':')[1]
+	Cell_id_all_new.append(j_new)
+
+print('trouble shooting: cell_id format in loom is ' + Cell_id_all_new[1])
+
+
+#############################
+Cell_id_all_new = pd.DataFrame(Cell_id_all_new)
+index = Cell_id_all_new.isin(cell_id_new).iloc[:,0]
+
+data_cl = data[index,:]
+
+print('dim of cleaned loom is ' + str(data_cl.shape[0]) + ' ' + str(data_cl.shape[1]))
+
+
+###### add UMAPs to data_cl ########
+
+Cell_id_all_new_cl = Cell_id_all_new[index].iloc[:,0].tolist()
+
+
+cell_id_new = list(cell_id_new)
+
+###### 
+
+Cell_index_FN = list()
+
+#####for i, n in enumerate(Cell_id_all_new_cl[1:5]): 
+#####	print(i)
+#####	print(n) cell_id_new.index(Cell_id_all_new_cl[1])
+
+for i, n in enumerate(Cell_id_all_new_cl): 
+	Cell_index_FN.append(cell_id_new.index(n))
+
+##### sort UMAP ##############
+UMAP = Meta_data.iloc[:,0:2]
+
+UMAP_M = UMAP.to_numpy()
+UMAP_K = UMAP_M[Cell_index_FN,]
+data_cl.obsm['X_umap'] = UMAP_K
+
+########################################
+#### run velocity on data_cl ###########
+########################################
+
+scv.utils.show_proportions(data_cl)
+scv.pp.normalize_per_cell(data_cl)
+scv.pp.filter_genes_dispersion(data_cl, n_top_genes=1000)
+scv.pp.log1p(data_cl)
+scv.pp.moments(data_cl, n_pcs=25, n_neighbors=50)
+scv.tl.recover_dynamics(data_cl)
+scv.tl.velocity(data_cl,mode='dynamical')
+scv.tl.velocity_graph(data_cl)
+
+output_file = output_tags + '_velocity.png'
+
+scv.pl.velocity_embedding_grid(data_cl,basis='umap',color="lightgrey",arrow_length=1, arrow_color='#CB2314',arrow_size=2,density=1,alpha=1,size=2.5,dpi=600,save=output_file)
+
+####################
+print('Done!!!')
+
+
+
